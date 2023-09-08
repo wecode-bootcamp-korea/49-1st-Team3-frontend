fa24f63c545e6728d8062e2a664ba02e1f804c15
Leechengwon <leecwee8497@gmail.com>
2023-09-08 18:16:06 +0900

    초기세팅

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Login.scss";
+
+const Login = () => {
+  return (
+    <div>
+      <div>초기페이지</div>
+    </div>
+  );
+};
+export default Login;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <div>
+      <div>초기페이지</div>
+    </div>
+  );
+};
+export default Main;
